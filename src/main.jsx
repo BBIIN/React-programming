eaca7f61da8adbe836d162ab402b4b480a832b86
BBIIN <tiktok0416@hanmail.net>
2023-08-09 14:12:40 +0900

    React 함수 컴포넌트의 요건
    - 함수 이름은 첫글자가 대문자!
    - JSX를 반환!

--- src/main.jsx
+++ src/main.jsx
@@ -1,150 +1,31 @@
-/* eslint-disable no-inner-declarations */
-/* React, ReactDOM 모듈 불러오기 및 버전 확인 -------------------------------------- */
+import { createRoot } from "react-dom/client";
 
-// React 모듈을 불러오기
-import * as React from "react";
+// React 함수 컴포넌트의 요건
+// - 함수 이름은 첫글자가 대문자!
+// - JSX를 반환!
 
-// ReactDOM 모듈을 불러오기
-import * as ReactDOM from "react-dom";
-
-// React 버전 확인하기
-// console.log(`React version: ${React.version}`);
-
-// ReactDOM 버전 확인하기
-// console.log(`ReactDOM version: ${ReactDOM.version}`);
-
-/* React 요소란? --------------------------------------------------------------- */
-
-// React 요소 작성 (React API)
-const visualBoldElement = React.createElement("b", null, "어썸(awesome)");
-
-const headlineElement = React.createElement(
-  "h1",
-  {
-    title: "React is Awesome!",
-  },
-  "React는",
-  visualBoldElement,
-  "해~"
-);
-
-const domAbbrElement = React.createElement(
-  "abbr",
-  { title: "Document Object Model" },
-  "DOM"
-);
-
-const uiAbbrElement = React.createElement(
-  "abbr",
-  { title: "User Interface" },
-  "UI"
-);
-
-const descriptionForReactElement = React.createElement(
-  "p",
-  {},
-  "React는 가상 ",
-  domAbbrElement,
-  "을 사용하는 ",
-  uiAbbrElement,
-  "라이브러리입니다."
-);
-
-const appElement = React.createElement(
-  /* type */
-  "div",
-  /* props {} */
-  {
-    className: "App",
-    id: "reactAppElement",
-    "data-type": "React.ReactElement",
-  },
-  /* ...children -> [child, child, child, ...] */
-  headlineElement,
-  descriptionForReactElement
-);
-
-// appElement(rootElement) tree
-// console.log('appElement\n', appElement);
-
-/* JSX를 사용하는 이유 ------------------------------------------------------------ */
-
-// React 요소 작성 (with JSX)
-const appElementJSX = (
-  <div className="App" id="reactAppElement" data-type="React.ReactElement">
-    <h1 title="React is Awesome!">
-      React는 <b>어썸(awesome)</b>해~
-    </h1>
-    <p>
-      React는 가상
-      <abbr title="Document Object Model">DOM</abbr>을 사용하는
-      <abbr title="User Interface">UI</abbr>
-      라이브러리입니다.
-    </p>
-  </div>
-);
-
-// console.log('appElementJSX\n',appElementJSX);
-
-/* React 요소를 재사용하기 위한 함수 작성 --------------------------------------------- */
-
-{
-  // const domAbbrElement = React.createElement(
-  //   'abbr',
-  //   { title: 'Document Object Model'},
-  //   'DOM'
-  // );
-
-  // const uiAbbrElement = React.createElement(
-  //   'abbr',
-  //   { title: 'User Interface'},
-  //   'UI'
-  // );
-
-  // - [ ] React 요소를 반환하는 함수 만들기
-  //   1. 함수 작성 createAbbrElement()
-  function createAbbrElement(props, ...children) {
-    return React.createElement("abbr", props, ...children);
-  }
-  // 아래 JSX 코드는 react 요소를 생성
-  // 그런데 웹 브라우저는 아래 코드가 해석이 안됨.
-  //<abbr title="Document Ovject model">DOM</abbr>
-
-  const domAbbrElement = createAbbrElement(
-    {
-      title: "Document Object Model",
-    },
-    "D",
-    "O",
-    "M"
+// App 함수 컴포넌트를 작성합니다.
+function App() {
+  // JSX 값 반환
+  return (
+    <div>
+      <h1>React</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum ab
+        animi, labore facilis obcaecati ullam placeat molestiae. Enim,
+        consectetur laudantium.
+      </p>
+      <ul>
+        <li>list item A</li>
+        <li>list item B</li>
+        <li>list item C</li>
+      </ul>
+    </div>
   );
-
-  console.log(domAbbrElement);
-
-  const uiAbbrElement = createAbbrElement(
-    {
-      title: "User Interface",
-    },
-    "UI"
-  );
-  console.log(uiAbbrElement);
-
-  // - [ ] 인수를 전달해 재사용 가능하도록 구현
-  //   2. createAbbrElement({ props: { title: '...' } }, '...');
-
-  // Abbr 함수 컴포넌트 만들기 (react용)
-  function Abbr(props /* {title, children} */) {
-    return <abbr title={props.title}>{props.children}</abbr>;
-  }
-
-  console.log(React.cloneElement(Abbr));
 }
 
-/* React 요소 vs. 컴포넌트 -------------------------------------------------------- */
-
-// App 함수 컴포넌트 만들기
-
-/* React 요소 트리를 DOM에 렌더링하려면? ---------------------------------------------- */
-
-// ReactDOM.createRoot() 를 사용해서 DOM 요소를 ReactDOMRoot 객체로 생성
-// ReactDOMRoot.render() 메서드로 App을 화면에 표시(렌더링)
+createRoot(document.getElementById("root")).render(
+  // App 컴포넌트를 렌더링할 수 있도록 JSX 구문을 추가합니다.
+  <App />
+  // <App></App>
+);
