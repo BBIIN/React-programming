5d63112665bebe96ba29bc5326721f463e9562c6
BBIIN <tiktok0416@hanmail.net>
2023-08-23 13:06:45 +0900

    상태 끌어올리기, Props 드릴링 이슈 (with 디바운싱)

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -15,6 +15,12 @@ function RootLayout({ displaySideMenu = false }) {
               <li>
                 <Link to="/learn/01">Props 전달</Link>
               </li>
+              <li>
+                <Link to="/learn/02">상태 끌어올리기</Link>
+              </li>
+              <li>
+                <Link to="/learn/03">속성(props) 드릴링 이슈</Link>
+              </li>
             </ul>
           </nav>
         )}
--- src/learn/2-lifting-state-up.jsx.jsx
+++ src/learn/2-lifting-state-up.jsx.jsx
@@ -1,5 +1,55 @@
+import debounce from '@/utils/debounce';
+import { useState } from 'react';
+
 function LiftingStateUp() {
-  return <></>;
+  // 상태
+  const [color, setColor] = useState('#999');
+
+  // 상태 업데이트 이벤트 핸들러
+  const handleChangeBgColor = (newColor) => setColor(newColor);
+
+  return (
+    <div className="PassingProps">
+      <Parent color={color} onChangeColor={handleChangeBgColor} />
+      <OtherParent color={color} />
+    </div>
+  );
 }
 
 export default LiftingStateUp;
+
+/* -------------------------------------------------------------------------- */
+
+function OtherParent({ color }) {
+  return (
+    <div
+      className="Parent flex justify-center items-center bg-slate-200 p-4"
+      style={{ backgroundColor: color }}
+    >
+      <p className="=">change Background Color</p>
+    </div>
+  );
+}
+
+export function Parent({ color, onChangeColor }) {
+  return (
+    <div className="Parent">
+      <Child color={color} onChangeColor={onChangeColor} />
+    </div>
+  );
+}
+
+export function Child({ color, onChangeColor }) {
+  return (
+    <div className="Child flex items-center justify-center gap-8">
+      <p className="text-4xl font-extralight uppercase" style={{ color }}>
+        Child
+      </p>
+      <input
+        type="color"
+        aria-label="글자 색상 변경"
+        onChange={debounce((e) => onChangeColor(e.target.value), 500)}
+      />
+    </div>
+  );
+}
