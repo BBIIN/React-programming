b91f4b79d8e7d5ab394c18f2f3c6381a8f09f378
BBIIN <tiktok0416@hanmail.net>
2023-08-24 13:11:14 +0900

    Web Animation API 명령형으로 제어
    - useRef() 기억(memo)
    - play()
    - pause()
    - stop()

--- src/routes.jsx
+++ src/routes.jsx
@@ -15,8 +15,11 @@ import LiftingStateUp from './learn/2-lifting-state-up.jsx';
 import PropsDrillingIssue from './learn/3-props-drilling-issue.jsx';
 import ReactContextAPI1 from './learn/4-1-react-context-api';
 import ReactContextAPI2 from './learn/4-2-seperation-react-context';
-import RefExampleMemoValues from './learn/5-ref-1-mema-values';
-import RefExampleReferencingDOM from './learn/6-ref-2-referencing-dom';
+import RefExampleMemoValues from './learn/5-ref-mema-values';
+import RefExampleReferencingDOM from './learn/6-ref-referencing-dom';
+import GSAP_Animation from './learn/7-ref-3-gsap-animation';
+import GSAP_Context from './learn/7-2-ref-gsap-context.jsx';
+import FramerMotion_Animation from './learn/8-framer-motion-declaration-animation';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -35,6 +38,9 @@ const router = createBrowserRouter(
       <Route path="learn/04/02" element={<ReactContextAPI2 />} />
       <Route path="learn/05" element={<RefExampleMemoValues />} />
       <Route path="learn/06" element={<RefExampleReferencingDOM />} />
+      <Route path="learn/07/01" element={<GSAP_Animation />} />
+      <Route path="learn/07/02" element={<GSAP_Context />} />
+      <Route path="learn/08" element={<FramerMotion_Animation />} />
     </Route>
   )
 );
