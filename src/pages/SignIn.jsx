297f0f82e2a1c20d99a48a37fa2ec4e181080e30
BBIIN <tiktok0416@hanmail.net>
2023-08-28 13:04:55 +0900

    아토믹 컴포넌트 추출 - Form Input, useStorage 커스텀 훅 리팩터링

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
-import { useNavigate, Link, useLocation } from 'react-router-dom';
 import pb from '@/api/pocketbase';
-import debounce from '@/utils/debounce';
+import { FormInput } from '@/components';
 import { useAuth } from '@/contexts/Auth';
+import debounce from '@/utils/debounce';
+import { useState } from 'react';
+import { Helmet } from 'react-helmet-async';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 function SignIn() {
   const { state } = useLocation();
@@ -41,67 +43,89 @@ function SignIn() {
   }, 400);
 
   return (
-    <div>
-      <h2>로그인 폼</h2>
+    <>
+      <Helmet>
+        <title>Sign In - ReactBird</title>
+      </Helmet>
+      <div className="container max-w-lg mx-auto">
+        <h2 className="my-5 text-2xl font-medium text-blue-950 text-center dark:text-sky-500/90">
+          로그인 폼
+        </h2>
 
-      <form
-        onSubmit={handleSignIn}
-        className="flex flex-col gap-2 mt-2 justify-start items-start"
-      >
-        <div>
-          <label htmlFor="email">이메일</label>
-          <input
+        <form
+          onSubmit={handleSignIn}
+          className="flex flex-col gap-2 items-center"
+        >
+          <FormInput
             type="email"
+            label="이메일"
             name="email"
-            id="email"
             defaultValue={formState.email}
             onChange={handleInput}
-            className="border border-slate-300 ml-2"
           />
-        </div>
-        <div>
-          <label htmlFor="password">패스워드</label>
-          <input
+          <FormInput
             type="password"
+            label="패스워드"
             name="password"
-            id="password"
             defaultValue={formState.password}
             onChange={handleInput}
-            className="border border-slate-300 ml-2"
           />
-        </div>
-        <div className="flex gap-2">
-          <button type="submit" className="disabled:cursor-not-allowed">
-            로그인
-          </button>
-          <button type="reset">취소</button>
-        </div>
-      </form>
 
-      <Link to="/signup">회원가입</Link>
-      {isAuth && (
-        <button
-          type="button"
-          className="ml-4"
-          onClick={async () => {
-            if (confirm('정말 탈퇴하실 건가요??')) {
-              if (pb.authStore.model) {
-                try {
-                  await pb.collection('users').delete(pb.authStore.model.id);
-                  console.log('탈퇴 성공');
-                } catch (error) {
-                  console.error(error);
+          <div className="flex gap-2 mt-5">
+            <button
+              type="submit"
+              className="
+                py-1 px-3.5 border-2 border-zinc-300 hover:border-zinc-400 rounded-full
+              dark:text-sky-400 dark:border-sky-400 dark:border-[1px] dark:hover:bg-sky-400 dark:hover:text-sky-50 dark:hover:border-sky-500
+              "
+            >
+              로그인
+            </button>
+            <button
+              type="reset"
+              className="
+                py-1 px-3.5 border-2 border-zinc-200 bg-zinc-200 hover:bg-zinc-300 hover:border-zinc-300 rounded-full
+                dark:bg-zinc-400 dark:border-zinc-400
+              "
+            >
+              취소
+            </button>
+          </div>
+        </form>
+
+        <div className="flex justify-center mt-8 border-t border-slate-200 pt-8 dark:border-slate-200/30">
+          <Link
+            to="/signup"
+            className="dark:text-zinc-500 dark:hover:text-zinc-300"
+          >
+            회원가입
+          </Link>
+        </div>
+
+        {isAuth && (
+          <button
+            type="button"
+            className="ml-4"
+            onClick={async () => {
+              if (confirm('정말 탈퇴할 생각인가요?')) {
+                if (pb.authStore.model) {
+                  try {
+                    await pb.collection('users').delete(pb.authStore.model.id);
+                    console.log('탈퇴 성공');
+                  } catch (error) {
+                    console.error(error);
+                  }
+                } else {
+                  console.log('현재 로그인 된 사용자가 없어요.');
                 }
-              } else {
-                console.log('현재 로그인 된 사용자가 없어요.');
               }
-            }
-          }}
-        >
-          탈퇴
-        </button>
-      )}
-    </div>
+            }}
+          >
+            탈퇴
+          </button>
+        )}
+      </div>
+    </>
   );
 }
 
